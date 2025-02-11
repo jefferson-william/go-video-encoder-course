d75944a6a60e041f7542298dd33262a2c4a911bf
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2025-02-11 15:45:00 -0300

    feat: workers & channel

--- main.go
+++ main.go
@@ -6,14 +6,27 @@ import (
 	"time"
 )
 
-func hello(msg string) {
-	fmt.Println(msg + "- goroutine")
-	time.Sleep(time.Duration(rand.Intn(1000)) * time.Second)
+func ProcessWorker(in chan int, worker int) {
+	for x := range in {
+		t := time.Duration(rand.Intn(10) * int(time.Second))
+		time.Sleep(t)
+		fmt.Println("Worker ", worker, ": ", int(x))
+	}
 }
 
 func main() {
-	go hello("hello 1")
-	go hello("hello 2")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Chamada normal")
+	concurrency := 8
+	in := make(chan int)
+	done := make(chan []byte)
+	go func() {
+		i := 0
+		for {
+			in <- 1
+			i++
+		}
+	}()
+	for x := 0; x < concurrency; x++ {
+		go ProcessWorker(in, x)
+	}
+	<- done
 }
